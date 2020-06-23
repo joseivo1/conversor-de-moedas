2f3363a34496e112fd6fd42d6a1e39609c8fc051
jose_ivo <joseivosobrinho@gmail.com>
2020-06-23 12:12:10 -0300

    Form Base to receive to API

--- src/app/conversor/components/conversor.component.ts
+++ src/app/conversor/components/conversor.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { Moeda, Conversao, ConversaoResponse } from "../models";
+import { MoedaService, ConversorService } from "../services";
 
 @Component({
-  selector: 'app-conversor',
-  templateUrl: './conversor.component.html',
-  styleUrls: ['./conversor.component.css']
+  selector: "app-conversor",
+  templateUrl: "./conversor.component.html",
+  styleUrls: ["./conversor.component.css"],
 })
 export class ConversorComponent implements OnInit {
+  moedas: Moeda[];
+  converao: Conversao[];
+  possuiErro: boolean;
+  ConversaoResponse: ConversaoResponse;
 
-  constructor() { }
+  @ViewChild("conversaoForm", { static: true })
+  conversaoFrom: NgForm;
 
-  ngOnInit() {
-  }
+  constructor(
+    private moedaService: MoedaService,
+    private conversorService: ConversorService
+  ) {}
 
+  ngOnInit() {}
 }
--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,13 +1,14 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
 import { ConversorComponent } from "./components";
 import { MoedaService } from "./services";
 import { ConversorService } from "./services";
 @NgModule({
   declarations: [ConversorComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   providers: [MoedaService, ConversorService],
   exports: [ConversorComponent],
 })
